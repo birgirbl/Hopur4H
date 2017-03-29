130652a540051ca19da6979b76fd3727b8198e8e
earnarson <etha10@hi.is>
2017-03-29 16:53:30 +0000

    Update Review.java

--- Review.java
+++ Review.java
@@ -1,25 +1,27 @@
 public class Review{
-
-private String name;
-private String reviewText;
-private Date date;  
-
-public Review getReviewText { 
-return reviewText;	
-}
-public void setReviewText(String reviewText){
-	this.reviewtext=reviewtext;
-}
-public String getName(){
-	return name;
-}
-public void setName(String name)
-{
-	this.name=name;
-}
-public Date getDate(){
-	return date;
-}
-//smidur skrifa inn
-//Review(traveler:Traveler,reviewText:String)
+	private String name;
+	private String reviewText;
+	private Date date;  
+	
+	public 	Review(Traveler traveler, String reviewText) {
+		this.name = traveler.getName();
+		this.reviewText = reviewText;
+		this.date = new Date();
+	}
+	public Review getReviewText { 
+		return reviewText;	
+	}
+	public void setReviewText(String reviewText){
+		this.reviewtext=reviewtext;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public Date getDate(){
+		return date;
+	}
 }
