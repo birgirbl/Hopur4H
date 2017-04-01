cf7199239c2146fccab492f6d7c61bf3c2419d02
birgirbl <bba8@hi.is>
2017-04-01 12:46:26 +0000

    Add files via upload

---
+++ HotelStorage.java
@@ -0,0 +1,31 @@
+public class HotelStorage {
+  
+ private List<Hotel> hotelListi = new List<Hotel>();
+ 
+ public Hotel getHotelListi(){
+   return this.hotelListi;
+ }
+ 
+ public void setHotelToDatabase(Hotel hotel){
+   this.hotel = hotel;
+ }
+ 
+ public void getHotelFromDatabase(String leitarord) {
+   
+ }
+ 
+ public void getHotelInfoFromDatabase(){
+   
+ }
+ 
+ public void setHotelInfoToDatabase(){
+   
+ }
+ 
+ public void deleteHotelFromDatabase(){
+   
+ }
+ 
+ public HotelStorage(){}
+ 
+}
