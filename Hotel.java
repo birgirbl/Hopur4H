b540f3797628adf1a58020f768f1922c1d9c1937
earnarson <etha10@hi.is>
2017-04-11 15:15:50 +0000

    Add files via upload

--- Hotel.java
+++ Hotel.java
@@ -1,57 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hotel{
 
-private String hotelName;
-private Rooms[]; //athuga með tagið, þetta er listi eða fylki
-private String location;
-private Listi review;  //athuga með tagið, þetta er listi eða fylki
-private Picture[] picture; //athuga með tagið, þetta er listi eða fylki
-
-public List getReview { //athuga með tagið, þetta er listi eða fylki
-return review;	
-}
-public void setReview(Traveler traveler, String reviewText){
-	this.traveler=traveler;
-	this.reviewtext=reviewtext;
-}
-public void setPic(Picture picture){
-	this.picture=picture;
-}
-public Picture getPic(){
-	return picture;
-}
-public void setRoom(String roomType,int price){
-	this.roomtype=roomtype;
-	this.price=price;
-}
-public String getRoom(){
-	return room;
-}
-public void setLocation(String city)
-{
-	this.location=location;
-}
-public String getLocation(){
-	return location;
-}
-//smidir skrifa inn
-//Hotel(hotelName:String,location: String)
-//Hotel(hotelName:String)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	private String hotelName;
+	private ArrayList<Room> rooms; //athuga með tagið, þetta er listi eða fylki
+	private String location;
+	private ArrayList<Review> review;  //athuga með tagið, þetta er listi eða fylki
+	private Picture picture; //athuga með tagið, þetta er listi eða fylki
+	private int quality;
+	
+	public String getHotelName() {
+		return hotelName;
+	}
+	
+	public ArrayList<Review> getReview() { //athuga með tagið, þetta er listi eða fylki
+		return review;	
+	}
+	
+	public void setReview(Traveler traveler, String reviewText){
+		review.add(new Review(traveler, reviewText));
+	}
+	
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+	
+	public int getQuality() {
+		return quality;
+	}
+	
+	public void setPic(Picture picture){
+		this.picture=picture;
+	}
+	
+	public Picture getPic(){
+		return picture;
+	}
+	public void setRoom(String roomType,int price){
+		Room x = new Room(roomType, price);
+		rooms.add(x);
+	}
+	public ArrayList<Room> getRooms(){
+		return rooms;
+	}
+	public void setLocation(String city)
+	{
+		this.location=location;
+	}
+	public String getLocation(){
+		return location;
+	}
+	
+	public void showPictures() {
+		
+	}
+	
+	public Hotel(String hotelName, String location) {
+		this.hotelName = hotelName;
+		this.location = location;
+		this.review = new ArrayList<Review>();
+		rooms = new ArrayList<Room>();
+	}
+	
+	public Hotel(String hotelName) {
+		this.hotelName = hotelName;
+		this.review = new ArrayList<Review>();
+		rooms = new ArrayList<Room>();
+	}
 
 }
--- HotelController.java
+++ HotelController.java
@@ -1,109 +1,131 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class HotelController {
-	private HotelStorage hS;
+	private HotelStorage hotelStorage;
+	private List<Hotel> hotelListi;
 	
 	public HotelController() {
-		hS = new HotelStorage();
+		hotelStorage = new HotelStorage();
+		hotelListi = hotelStorage.getHotelListi();
 	}
 	
 	public void search(String leitarord) {
-		hS.getHotelFromDatabase(leitarord);
-		//displayResults();
+		hotelStorage.getHotelFromDatabase(leitarord);
 	}
 	
-	public String displayResults(){
-		if(hS.getHotelListi().size() == 0) {
-			return "Engar niðurstöður fundust";
+	public void displayResults(){
+		String[] hotel = new String[hotelListi.size()];
+		
+		if(hotelListi.size() == 0) {
+			System.out.println("Engar niðurstöður fundust");
 		}
-		for(int i=0; i<hS.getHotelListi().size(); i++) {
-			String hotelNafn = hS.getHotelListi().get(i).getHotelName();
-			return hotelNafn;
-		}
-		return "";
-	}
-	
-	public void displayHotelInformation(Hotel hotel) {
-		List<Room> x = hS.getRoomsFromDatabase(hotel);
-		for(int i=0; i<x.size(); i++) {
-			String roomType = x.get(i).getRoomType();
-			int price = x.get(i).getPrice();
-			System.out.println(roomType + " : " + price);
+		int count = 0;
+		for(int i=0; i<hotelListi.size(); i++) {
+			String hotelNafn = hotelListi.get(i).getHotelName();
+			System.out.println(hotelNafn);
 		}
 	}
 	
-	
-	public String filter(String filter, int tala) {
-		
-		if(filter.equals("price")) {
-			
-			
-			
-		}
-		
-		return filter;
-	}
-
-	public void deleteHotel(String hotelName, String location) {
-		//hS.deleteHotelFromDatabase();
-	}
-	
-	////athuga
-	public void overwrite(String , String ) {
-		
-	}
-	
-	////athuga
-	public void delete(String , String ) {
-		//bS.deleteBookingFromDatabase();
-	}
-	
-	////athuga
-	public void createBooking(Hotelname Hotel, Room room, Traveler traveler, Date dateFrom, Date dateTo) {
-		bS.setBookingToDatabase(hotel, room, traveler, dateFrom, dateTo);
-	}
-	
-	////athuga
-	public boolean isRoomAvaivable() {
-		
-	}
-	
-	
-	
-	public void createReview(Traveler traveler, Hotel hotel, String review) {
-		hS.setReviewToDatabase(traveler, hotel, review);
-	}
-	
-	public void showPictures(Hotel hotel) {
-		for(int i=0; i<hS.getHotelListi().size(); i++) {
-			if(hS.getHotelListi().get(i).equals(hotel)) {
-				hS.getHotelListi().get(i).showPictures();
+	public void displayReviews(String hotelName) {
+		for(int i=0; i<hotelListi.size(); i++) {
+			if(hotelListi.get(i).getHotelName().equals(hotelName)) {
+				List<Review> reviewListi = hotelListi.get(i).getReview();
+				for(Review x : reviewListi) {
+					System.out.println(x.toString());
+				}
+				
 			}
 		}
 	}
-
-
 	
-	public String sort(String sort) {
-		return sort;
+	public void filter(String filter, int tala) {
+		String[] hotel = new String[hotelListi.size()];
+		int count = 0;
+		if(filter.toLowerCase().equals("price")) {
+			for(int i=0; i<hotelListi.size(); i++) {
+				List<Room> roomList = hotelListi.get(i).getRooms();
+				if(priceFilter(roomList, tala)) {
+					System.out.println(hotelListi.get(i).getHotelName());
+				}
+			}
+		}
+		if(filter.toLowerCase().equals("quality")) {
+			for(int i=0; i<hotelListi.size(); i++) {
+				if(hotelListi.get(i).getQuality() <= tala) {
+					System.out.println(hotelListi.get(i).getHotelName());
+				}
+			}
+		}
+	}
+	
+	public boolean priceFilter(List<Room> roomList, int tala) {
+		for(int i=0; i<roomList.size(); i++) {
+			if(roomList.get(i).getPrice() < tala) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void deleteHotel(String hotelName, String location) {
+		hotelStorage.deleteHotelFromDatabase();
+	}
+	
+	public void createReview(Traveler traveler, Hotel hotel, String review) {
+		hotelStorage.setReviewToDatabase(traveler, hotel, review);
+	}
+	
+	public void showPictures(Hotel hotel) {
+		for(int i=0; i<hotelListi.size(); i++) {
+			if(hotelListi.get(i).equals(hotel)) {
+				hotelListi.get(i).showPictures();
+			}
+		}
+	}
+	
+	public void sort(String sorteraEftir, String order) {
+		if(sorteraEftir.equals("price")) {
+	        Collections.sort(hotelListi,new Comparator<Hotel>() {
+	            @Override
+	            public int compare(Hotel h1, Hotel h2) {
+	                Integer h1Min = findLowestPrice(h1.getRooms());
+	                Integer h2Min = findLowestPrice(h2.getRooms());
+	            	return h1Min.compareTo(h2Min);
+	            }
+	        });
+		}
+		if(sorteraEftir.equals("quality")) {
+	        Collections.sort(hotelListi,new Comparator<Hotel>() {
+	            @Override
+	            public int compare(Hotel h1, Hotel h2) {
+	                Integer h1Min = h1.getQuality();
+	                Integer h2Min = h2.getQuality();
+	            	return h1Min.compareTo(h2Min);
+	            }
+	        });
+		}
+        if(order.equals("descending")) {
+        	Collections.reverse(hotelListi);
+        }
+	}
+
+	private int findLowestPrice(List<Room> roomList) {
+		int min = Integer.MAX_VALUE;
+		for(int i=0; i<roomList.size(); i++) {
+			if(roomList.get(i).getPrice() < min) {
+				min = roomList.get(i).getPrice();
+			}
+		}
+		return min;
 	}
 	
 	public static void main(String[] args) {
 		HotelController hotelController = new HotelController();
-		System.out.println(hotelController.displayResults());
+		hotelController.sort("quality", "descending");
+		hotelController.displayResults();
+		//hotelController.filter("quality", 4);
 	}
-	
-	
-	
-	
-	
-	
-	
-	// Hafa þetta sem sækja info f. allan listann eða bara hvert tilvik
-	// fyrir sig í einu
-	//hS.getHotelInfoFromDatabase();
-	// eða þá 
-	// hS.getHotelList().get(i).getHotelInfoFromDatabase();
-	
-	// getReview, getRoom etc.. sækja annars allt um hótel fyrst
 }
